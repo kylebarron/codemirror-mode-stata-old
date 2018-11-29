293c2dae57c39fdb596c01a6aeaccb035517e5ac
Kyle Barron <barronk@mit.edu>
2018-11-29 13:24:04 -0500

    import codemirror object from outside

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
+import CodeMirror from 'codemirror';
 
-
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"), require("../../addon/mode/simple"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "../../addon/mode/simple"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 // "use strict";
 
 var builtins_base = [
@@ -499,4 +491,3 @@ CodeMirror.defineSimpleMode("stata",{
 
 CodeMirror.defineMIME('text/x-stata', 'stata');
 CodeMirror.defineMIME('text/stata', 'stata');
-});
